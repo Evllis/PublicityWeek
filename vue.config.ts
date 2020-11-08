21b80d1490963bf7be6275de5cf1e3909d1a6332
Evllis <7284395@qq.com>
2020-11-08 19:03:23 +0800

    first commit

---
+++ vue.config.ts
@@ -0,0 +1,50 @@
+import { resolve } from 'path'
+
+import { isDevFn, loadEnv } from './build/utils'
+
+const pkg = require('./package.json')
+
+const vueEnv = loadEnv()
+
+const {
+    VUE_APP_PORT,
+    VUE_APP_PUBLIC_PATH,
+    VUE_APP_PROXY,
+    VUE_APP_DROP_CONSOLE,
+    VUE_APP_OUT_DIR,
+    // VUE_APP_USE_CDN,
+} = vueEnv
+
+module.exports = {
+    // 部署应用包时的基本 URL(解决build打包后资源文件404问题) :https://cli.vuejs.org/zh/config/#baseurl
+    publicPath: VUE_APP_PUBLIC_PATH || './',
+    outputDir: VUE_APP_OUT_DIR || 'dist', // 'dist', 生产环境构建文件的目录
+    assetsDir: 'static', // 静态资源生成目录（相对于outputDir
+    productionSourceMap: false, // 关闭线上源码
+    // css相关配置
+    // 图片在10kb以内使用内联base64图片
+    chainWebpack: config => {
+        config.module
+            .rule('images')
+            .use('url-loader')
+            .loader('url-loader')
+            .tap(options => Object.assign(options, {
+                limit: 10240
+            }));
+    },
+    devServer: {
+        open: true,
+        compress: true,
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        proxy: null
+    },
+    // 关闭生产环境console
+    configureWebpack(config) {
+        if (process.env.NODE_ENV === 'production') {
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
+        }
+    }
+};
